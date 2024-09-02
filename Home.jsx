9c867851bba8352d5b49aa189bdf5a585a5f332d
VinamraSuman17 <vinamrasuman33@gmail.com>
2024-09-03 01:06:57 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './ui/Navbar'
+import HeroSection from './HeroSection'
+import CategoryCarousel from './CategoryCarousel'
+import LatestJobs from './LatestJobs'
+import Footer from "./Footer"
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <HeroSection/>
+      <CategoryCarousel/>
+      <LatestJobs/>
+      <Footer/>
+
+
+    </div>
+  )
+}
+
+export default Home
